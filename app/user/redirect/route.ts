35c788c87f972f50969068911ea48cc924c5e1bb
rakeshsenapathi <rakeshsenapathi@gmail.com>
2024-09-07 09:55:08 +0530

    fix: eslint error

--- app/user/redirect/route.ts
+++ app/user/redirect/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../../api/auth/[...nextauth]/route';
+import { authOptions } from '@/auth';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
